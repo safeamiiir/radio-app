ab52364efb14c85c1109c9ceb81439f90dd65bc5
Olena Reukova <kronverk01@gmail.com>
2023-08-22 02:42:59 +0100

    fixing the mixed content issue
    To fix the mixed content issue,  make sure that the radio station URLs are fetched over HTTPS.
    By using .replace(/^http:/, 'https:'), we are modifying the URLs to use HTTPS instead of HTTP. This change ensures that the radio station URLs are fetched over a secure connection.
    
    Installed js-cookie

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import Cookies from 'js-cookie';
 import Radio from './Radio';
 import Hero from './Hero';
 import Footer from './Footer';
 
 function App() {
+  useEffect(() => {
+    Cookies.set('yourCookieName', 'yourCookieValue', {
+      sameSite: 'none',
+      secure: true,
+    });
+
+    Cookies.set('anotherCookie', 'anotherValue', { sameSite: 'strict' });
+
+    Cookies.set('yetAnotherCookie', 'yetAnotherValue', { sameSite: 'lax' });
+  }, []);
   return (
     <div className='App'>
       <h1>Radio Player</h1>
