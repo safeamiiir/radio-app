6bcc0c4dfd55edfcdb47f87739e03eabb4b3366e
Olena Reukova <kronverk01@gmail.com>
2023-08-30 13:59:54 +0100

    edit footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  const linkedinURL = 'https://www.linkedin.com/in/olenareukova';
-
   return (
     <footer className='footer'>
       <div className='footer-left'>
